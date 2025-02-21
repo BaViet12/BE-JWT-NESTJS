6aa1b1ba79183735cfbcf6a3ce01544a5c806cb0
BaViet12 <hpa220377@gmail.com>
2025-02-21 22:33:22 +0700

    update src

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
-dotenv.config();
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const configService = app.get(ConfigService);
-
   const port = configService.get('PORT');
 
   await app.listen(port);
